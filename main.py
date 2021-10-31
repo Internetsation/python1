a0815cd496cb65c3783583dc692b10e143afb611
Internetsation <ericbetz.civ@gmail.com>
2021-10-30 23:57:19 -0400

    Feature2 adds the graphic output of deathstar plans with turtle import and changes Obiwan to do list.

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# Project 5 feature 1
+# Project 5 feature 2 provides to do list with tutrle graphic of deathstar plans.
 # This program asks for the user's name and greets them.
 # If the user is "Obiwan", they are asked to fight the empire!
 # Otherwise they are asked to deliver the droid to Obiwan.
 
+# import module
+from turtle import *
 
 # Get user name
-
-
-from random import randrange
-
 name = input("Tell me your name:")
-destruct = randrange(15)
 
 
 def my_function():
@@ -24,19 +21,23 @@ def my_function():
 def jedi_request(name):
     if name == "Obiwan":
         # If user is Obiwan help is requested
-        print(f' {name}-Kenobi \n Do not forget to tell Luke that Leia is his sister \n and Vader is his '
-              f'father!')
+        print(f' {name}-Kenobi To do list:\n 1.Tell Luke Leia is his sister \n 2. Vader is his father\n 3. Blow up '
+              f'Death Star')
+        # Prints deathstar plans
+        # set line color to red
+        color('red')
+        # draw a circle with radius of 50 pixels
+        circle(150)
+        # end the drawing
+        color('black')
+        circle(50)
+        done()
     else:
         # If user is not Obiwan help is requested
         print(f'{name} \n This is not the droid you are looking for! \n please return this droid to Obiwan-Kenobi!  ')
-
-
-def self_destruct():
-    print(f" This message will self destruct in {destruct} seconds")
 
 
 if __name__ == '__main__':
     # Run file
     my_function()
     jedi_request(name)
-    self_destruct()
