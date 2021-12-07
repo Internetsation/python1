55ef4b4a5d214b8254e9c097b91d7c3e9ba5cd9a
Eric <44822292+Internetsation@users.noreply.github.com>
2021-12-07 13:32:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,32 @@
-# Project 5!
-# This program asks for the user's name and greets them.
-# If the user is "Obiwan", they are asked to fight the empire!
-# Otherwise they are asked to deliver the droid to Obiwan.
+# Project 6! String 1
+# This program reminds Obiwan what he needs to do
+# If the user is not Obiwan they are asked to return the droids to him
+# Asks user to input a new reminder list*** WIP currently causes droids to explode!
+import sys
 
+jedi = sys.argv
 
-# Get user name
-name = input("Tell me your name:")
-
-
-def my_function():
-    # Print user greeting
-    while name == "Obiwan":
-        break
+reminders = "\n 1. These are your droids\n 2. Luke and Leia are siblings,Vader is their father\n 3. Buy blue milk"
+def droid_check():
+    # Remind Obiwan what he needs to do
+    if jedi == ['main.py', 'Obiwan']:
+        print(reminders)
+    # If the user is not Obiwan then instruct them to return his droids
     else:
-        print(f'Hello {name}')
+        print(f'Hello {jedi} \n These are not the droids you are looking for! \n Return them to Obiwan-Kenobi!')
 
-
-def jedi_request(name):
-    if name == "Obiwan":
-        # If user is Obiwan help is requested
-        print(f' {name}-Kenobi \n Do not forget to tell Luke that Leia is his sister \n and Vader is his '
-              f'father!')
+def new_to_do():
+    check = input("Would you like to create a new to do list?")
+    if check != "y":
+        print("May the force be with you")
     else:
-        # If user is not Obiwan help is requested
-        print(f'{name} \n This is not the droid you are looking for! \n please return this droid to Obiwan-Kenobi!  ')
+        remind1 = input("What do you need to remember?")
+        remind1 = remind1.swapcase()
+        print("~Translating to droid~"*10)
+        print(remind1*5,end="\nWARNING!\nDroid malfunction!\nFatal error!!\nSelf destruct imminent!!!!\nbeeEEoupp")
 
 
+# Run file
 if __name__ == '__main__':
-    # Run file
-    my_function()
-    jedi_request(name)
+    droid_check()
+    new_to_do()
